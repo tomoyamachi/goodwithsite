cee95823fdca83dfbb05fdfc95e1e5e4e80d4831
Tomoya AMACHI <tomoya.amachi@gmail.com>
2020-03-22 18:09:30 +0900

    fix rss script

--- src/lib/build-rss.ts
+++ src/lib/build-rss.ts
@@ -60,12 +60,12 @@ function createRSS(blogPosts = []) {
 
   return `<?xml version="1.0" encoding="utf-8"?>
   <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>My Blog</title>
-    <subtitle>Blog</subtitle>
+    <title>Goodwith Blog</title>
+    <subtitle>We design DevSecOps on Containers.</subtitle>
     <link href="/atom" rel="self" type="application/rss+xml"/>
     <link href="/" />
     <updated>${NOW}</updated>
-    <id>My Notion Blog</id>${postsString}
+    <id>Goodwith Blog</id>${postsString}
   </feed>`
 }
 
